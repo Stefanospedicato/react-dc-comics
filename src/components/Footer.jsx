364b8df4fd813864541b74cd2ca66bc670a93a04
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-13 15:48:54 +0100

    feat: add icons in blue footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,27 @@
 function Footer() {
   return (
     <footer>
-      <section className="container footer-icons">
-
+      <section className="container footer-icons justify-content-center">
+        <div className="d-flex mx-3 align-items-center">
+          <img src="./public/img/buy-comics-digital-comics.png" alt="DC" />
+          <p>DIGITAL COMICS</p>
+        </div>
+        <div className="d-flex mx-3 align-items-center">
+          <img src="./public/img/buy-comics-merchandise.png" alt="MERCH" />
+          <p>DC MERCHANDISE</p>
+        </div>
+        <div className="d-flex mx-3 align-items-center">
+          <img src="./public/img/buy-comics-subscriptions.png" alt="SUB" />
+          <p>SUBSCRIPTION</p>
+        </div>
+        <div className="d-flex mx-3 align-items-center">
+          <img src="./public/img/buy-comics-shop-locator.png" alt="SHOP" />
+          <p>COMIC SHOP LOCATOR</p>
+        </div>
+        <div className="d-flex mx-3 align-items-center">
+          <img src="./public/img/buy-dc-power-visa.svg" alt="VISA" />
+          <p>DC POWER VISA</p>
+        </div>
       </section>
       <section className="container footer-dc">
 
