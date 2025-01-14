60d50597d1ca9936862a40b20b89eefa66678285
Stefanospedicato <stefano.spedicato11@gmail.com>
2025-01-14 11:17:40 +0100

    feat: bonus complete

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,9 @@
+import Icons from './Icons'
+
 function Footer() {
   return (
     <footer>
-      <section className=" footer-icons">
-        <div className="container d-flex justify-content-between">
-          <div className=" d-flex mx-3 align-items-center">
-            <img src="./public/img/buy-comics-digital-comics.png" alt="DC" />
-            <p>DIGITAL COMICS</p>
-          </div>
-          <div className="d-flex mx-3 align-items-center">
-            <img src="./public/img/buy-comics-merchandise.png" alt="MERCH" />
-            <p>DC MERCHANDISE</p>
-          </div>
-          <div className="d-flex mx-3 align-items-center">
-            <img src="./public/img/buy-comics-subscriptions.png" alt="SUB" />
-            <p>SUBSCRIPTION</p>
-          </div>
-          <div className="d-flex mx-3 align-items-center">
-            <img src="./public/img/buy-comics-shop-locator.png" alt="SHOP" />
-            <p>COMIC SHOP LOCATOR</p>
-          </div>
-          <div className="d-flex mx-3 align-items-center">
-            <img src="./public/img/buy-dc-power-visa.svg" alt="VISA" />
-            <p>DC POWER VISA</p>
-          </div>
-
-        </div>
-      </section>
+      <Icons />
       <section className="footer-dc">
         <div className="container">
           <div className="dc-comics">
